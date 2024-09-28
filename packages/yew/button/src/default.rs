b9683057141f3509cf9c22c5424d33219ba7a73a
Christophe Billiard <christophe.billiard@gmail.com>
2024-09-28 18:42:40 +0200

    feat: add shadcn/ui Button in yew

---
+++ packages/yew/button/src/default.rs
@@ -0,0 +1,99 @@
+use std::rc::Rc;
+
+use tailwind_fuse::{tw_merge, AsTailwindClass};
+use yew::{prelude::*, virtual_dom::VTag};
+use yew_attrs::{attrs, Attrs};
+
+#[derive(Clone, PartialEq)]
+pub enum ButtonVariant {
+    Default,
+    Destructive,
+    Outline,
+    Secondary,
+    Ghost,
+    Link,
+}
+
+impl AsTailwindClass for ButtonVariant {
+    fn as_class(&self) -> &str {
+        match self {
+            ButtonVariant::Default => "bg-primary text-primary-foreground hover:bg-primary/90",
+            ButtonVariant::Destructive => {
+                "bg-destructive text-destructive-foreground hover:bg-destructive/90"
+            }
+            ButtonVariant::Outline => {
+                "border border-input bg-background hover:bg-accent hover:text-accent-foreground"
+            }
+            ButtonVariant::Secondary => {
+                "bg-secondary text-secondary-foreground hover:bg-secondary/80"
+            }
+            ButtonVariant::Ghost => "hover:bg-accent hover:text-accent-foreground",
+            ButtonVariant::Link => "text-primary underline-offset-4 hover:underline",
+        }
+    }
+}
+
+#[derive(Clone, PartialEq)]
+pub enum ButtonSize {
+    Default,
+    Sm,
+    Lg,
+    Icon,
+}
+
+impl AsTailwindClass for ButtonSize {
+    fn as_class(&self) -> &str {
+        match self {
+            ButtonSize::Default => "h-10 px-4 py-2",
+            ButtonSize::Sm => "h-9 rounded-md px-3",
+            ButtonSize::Lg => "h-11 rounded-md px-8",
+            ButtonSize::Icon => "h-10 w-10",
+        }
+    }
+}
+
+#[derive(Properties, PartialEq, Clone)]
+pub struct ButtonProps {
+    #[prop_or_default]
+    pub variant: Option<ButtonVariant>,
+    #[prop_or_default]
+    pub size: Option<ButtonSize>,
+    #[prop_or_default]
+    pub class: Option<String>,
+    #[prop_or_default]
+    pub node_ref: NodeRef,
+    #[prop_or_default]
+    pub attrs: Attrs,
+    pub children: Html,
+}
+
+#[function_component]
+pub fn Button(props: &ButtonProps) -> Html {
+    let base_class = "inline-flex items-center justify-center whitespace-nowrap rounded-md text-sm font-medium ring-offset-background transition-colors focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:pointer-events-none disabled:opacity-50";
+
+    let variant = props.variant.clone().unwrap_or(ButtonVariant::Default);
+    let size = props.size.clone().unwrap_or(ButtonSize::Default);
+    let class = props.class.clone().unwrap_or_default();
+
+    let combined_class = &tw_merge!(base_class.to_owned(), variant, size, &class);
+
+    let merged_attrs = use_memo(props.attrs.clone(), |attrs| {
+        attrs
+            .clone()
+            .merge(attrs! {
+               class={combined_class}
+            })
+            .expect("Attributes should be merged.")
+    });
+
+    let attrs = Rc::unwrap_or_clone(merged_attrs);
+
+    attrs
+        .new_vtag(
+            "button",
+            props.node_ref.clone(),
+            Default::default(),
+            props.children.clone(),
+        )
+        .into()
+}
