605bad9f218767eece551decaeac65fbad4327dc
Daniëlle Huisman <danielle@huisman.me>
2024-11-23 16:22:31 +0100

    Remove old Leptos component from registry

--- packages/registry/src/registry_ui.rs
+++ packages/registry/src/registry_ui.rs
@@ -1,33 +1,34 @@
 use std::{collections::HashMap, sync::LazyLock};
 
-use crate::schema::{FrameworkName, Registry, RegistryEntry, RegistryItemFile, RegistryItemType};
+use crate::schema::{FrameworkName, Registry};
 
 pub static UI: LazyLock<HashMap<FrameworkName, Registry>> = LazyLock::new(|| {
     HashMap::from([
         (FrameworkName::Dioxus, vec![]),
         (
             FrameworkName::Leptos,
-            vec![RegistryEntry {
-                name: "button".into(),
-                r#type: RegistryItemType::Ui,
-                description: None,
-                dependencies: Some(vec!["radix-leptos-slot".into()]),
-                dev_dependencies: None,
-                registry_dependencies: None,
-                files: Some(vec![RegistryItemFile {
-                    path: "ui/button.rs".into(),
-                    content: None,
-                    r#type: RegistryItemType::Ui,
-                    target: None,
-                }]),
-                tailwind: None,
-                css_vars: None,
-                source: None,
-                category: None,
-                subcategory: None,
-                chunks: None,
-                docs: None,
-            }],
+            vec![],
+            // RegistryEntry {
+            //     name: "button".into(),
+            //     r#type: RegistryItemType::Ui,
+            //     description: None,
+            //     dependencies: Some(vec!["radix-leptos-slot".into()]),
+            //     dev_dependencies: None,
+            //     registry_dependencies: None,
+            //     files: Some(vec![RegistryItemFile {
+            //         path: "ui/button.rs".into(),
+            //         content: None,
+            //         r#type: RegistryItemType::Ui,
+            //         target: None,
+            //     }]),
+            //     tailwind: None,
+            //     css_vars: None,
+            //     source: None,
+            //     category: None,
+            //     subcategory: None,
+            //     chunks: None,
+            //     docs: None,
+            // },
         ),
         (FrameworkName::Yew, vec![]),
     ])
