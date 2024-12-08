b586bef3acdf5a9c1fca1dd0286de48abc771011
Israel Barbara <israelsilvabarbara@gmail.com>
2024-12-08 18:41:25 -0300

    Add alert crate to leptos

---
+++ packages/leptos/alert/src/lib.rs
@@ -0,0 +1,8 @@
+//! Leptos port of [shadcn/ui Alert](https://ui.shadcn.com/docs/components/Alert).
+//!
+//! Displays a Alert or a component that looks like a Alert.
+//!
+//! See [the Rust shadcn/ui book](https://shadcn-ui.rustforweb.org/components/Alert.html) for more documenation.
+
+pub mod default;
+pub mod new_york;
