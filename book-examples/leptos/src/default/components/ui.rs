05d0815ebba7b56722960ebf88bb31d5f722852d
Manas Mahanand <robot14.mahanand@gmail.com>
2024-12-09 09:49:51 +0530

    added missing dependencies in card

--- book-examples/leptos/src/default/components/ui.rs
+++ book-examples/leptos/src/default/components/ui.rs
@@ -1,7 +1,6 @@
 // In actual projects this module would contain the copied components, but this example uses the local workspace packages.
 
-#[cfg(feature = "button")]
+#[cfg(any(feature = "button", feature = "card"))]
 pub use shadcn_ui_leptos_button::default as button;
-
 #[cfg(feature = "card")]
 pub use shadcn_ui_leptos_card::default as card;
