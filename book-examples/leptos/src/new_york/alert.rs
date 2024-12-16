66cca4a7bd17c2910d541e87d107a97fe23421b4
Israel Barbara <israelsilvabarbara@gmail.com>
2024-12-16 10:36:45 -0300

    Fix examples for new-york

--- book-examples/leptos/src/new_york/alert.rs
+++ book-examples/leptos/src/new_york/alert.rs
@@ -1,8 +1,6 @@
 #[allow(clippy::module_inception)]
 mod alert;
-mod alert_class;
 mod alert_destructive;
-mod alert_icon;
 
 use leptos::prelude::*;
 use leptos_router::{
@@ -16,8 +14,6 @@ pub fn AlertRoutes() -> impl MatchNestedRoutes + Clone {
         <ParentRoute path=path!("/alert") view=Outlet>
             <Route path=path!("/") view=alert::AlertDemo />
             <Route path=path!("/destructive") view=alert_destructive::AlertDestructive />
-            <Route path=path!("/icon") view=alert_icon::AlertIcon />
-            <Route path=path!("/class") view=alert_class::AlertClass />
         </ParentRoute>
     }
     .into_inner()
--- book-examples/leptos/src/new_york/alert/alert_destructive.rs
+++ book-examples/leptos/src/new_york/alert/alert_destructive.rs
@@ -1,3 +1,4 @@
+use lucide_leptos::CircleAlert;
 use leptos::prelude::*;
 
 use crate::new_york::components::ui::alert::{Alert, AlertDescription, AlertTitle, AlertVariant};
@@ -6,9 +7,10 @@ use crate::new_york::components::ui::alert::{Alert, AlertDescription, AlertTitle
 pub fn AlertDestructive() -> impl IntoView {
     view! {
         <Alert variant={AlertVariant::Destructive}>
-            <AlertTitle>"Heads up!"</AlertTitle>
+            <CircleAlert attr:class="h-4 w-4" />
+            <AlertTitle>"Error"</AlertTitle>
             <AlertDescription>
-                "You can add components to your app using the cli."
+                "Your session has expired. Please log in again."
             </AlertDescription>
         </Alert>
     }
