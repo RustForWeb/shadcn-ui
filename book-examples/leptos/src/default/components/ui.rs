6c7c5705de3b7d28690a418375df9a3d03942430
Israel Barbara <israelsilvabarbara@gmail.com>
2024-12-19 07:34:01 -0300

    book-examples new-york

--- book-examples/leptos/src/default/components/ui.rs
+++ book-examples/leptos/src/default/components/ui.rs
@@ -1,5 +1,6 @@
 // In actual projects this module would contain the copied components, but this example uses the local workspace packages.
-
+#[cfg(feature = "breadcrumb")]
+pub use shadcn_ui_leptos_breadcrumb::default as breadcrumb;
 #[cfg(any(feature = "button", feature = "card"))]
 pub use shadcn_ui_leptos_button::default as button;
 #[cfg(feature = "card")]
