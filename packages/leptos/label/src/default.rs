1917df5587740cea557f48040b5b83fabfc0a4a6
Israel Barbara <israelsilvabarbara@gmail.com>
2025-01-09 14:38:08 -0300

    Add default/new_york impl

--- packages/leptos/label/src/default.rs
+++ packages/leptos/label/src/default.rs
@@ -1,47 +1,31 @@
-use radix_yew_label::Label as LabelPrimitive;
+use radix_leptos_label::Label as LabelPrimitive;
+use shadcn_ui_leptos_utils::handlers::MaybeCallback;
 use tailwind_fuse::*;
-use yew::prelude::*;
-use yew_style::Style;
+use leptos::{ev::MouseEvent, prelude::*};
+use leptos_style::Style;
 
-#[derive(PartialEq, Properties)]
-pub struct LabelProps {
-    // Global attributes
-    #[prop_or_default]
-    pub class: Option<String>,
-    #[prop_or_default]
-    pub id: Option<String>,
-    #[prop_or_default]
-    pub style: Style,
+#[component]
+pub fn Label(
+    #[prop(into, optional)] class: MaybeProp<String>,
+    #[prop(into, optional)] id: MaybeProp<String>,
+    #[prop(into, optional)] style: Signal<Style>,
+    
+  // node_ref needs to be implemented into LabelPrimitive. Or implement IntoProperty,
+    //#[prop(into, optional)] node_ref: AnyNodeRef,
+    #[prop(into, optional)] r#for: MaybeProp<String>,
+    #[prop(into, optional)] on_mouse_down: MaybeCallback<MouseEvent>,
+    #[prop(optional)] children: Option<Children>,
 
-    // Attributes from `label`
-    #[prop_or_default]
-    pub r#for: Option<String>,
-
-    // Event handler attributes
-    #[prop_or_default]
-    pub on_mouse_down: Callback<MouseEvent>,
-
-    #[prop_or_default]
-    pub node_ref: NodeRef,
-    #[prop_or_default]
-    pub children: Html,
-}
-
-#[function_component]
-pub fn Label(props: &LabelProps) -> Html {
-    html! {
+) -> impl IntoView {
+    view! {
         <LabelPrimitive
-            class={tw_merge!("text-sm font-medium leading-none peer-disabled:cursor-not-allowed peer-disabled:opacity-70", &props.class)}
-            id={props.id.clone()}
-            style={props.style.clone()}
-
-            r#for={props.r#for.clone()}
-
-            on_mouse_down={props.on_mouse_down.clone()}
-
-            node_ref={props.node_ref.clone()}
+            attr:class=move || tw_merge!("text-sm font-medium leading-none peer-disabled:cursor-not-allowed peer-disabled:opacity-70", class.get())
+            attr:style=style.get().to_string()
+            attr:id=id.get()
+            attr:r#for=r#for.get()
+            on_mouse_down=on_mouse_down
         >
-            {props.children.clone()}
+            {children.map(|children| children())}
         </LabelPrimitive>
     }
 }
